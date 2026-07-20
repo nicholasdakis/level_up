b8ab63de04cf2c9492a9dcb19563602e0dbf88d6
Nicholas Dakis <n1ch0lasd4k1s@gmail.com>
2026-07-20 18:23:48 -0400

    Add import

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -3,3 +3,4 @@ from backend.services.food_service import FoodService
 from backend.services.poi_service import POIService
 from backend.services.progression_service import ProgressionService, experience_needed, calculate_daily_reward_xp, calculate_level_up
 from backend.services.snapshot_service import SnapshotService
+from backend.services.friendship_service import FriendshipService
